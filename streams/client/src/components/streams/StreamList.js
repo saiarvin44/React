1572070658908c7e04060c450a43d63f6ef2a999
saiarvin44 <saiarvind44@gmail.com>
2021-01-10 09:32:58 +0530

    chore: edit stream

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchStreams } from '../../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -12,15 +12,19 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
-          <button className="ui button negative">Delete</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link to="" className="ui button negative">
+            Delete
+          </Link>
         </div>
       );
     }
   }
 
   renderList() {
-    return this.props.streams.map(stream => {
+    return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
@@ -37,7 +41,7 @@ class StreamList extends React.Component {
   renderCreate() {
     if (this.props.isSignedIn) {
       return (
-        <div style={{ textAlign: 'right' }}>
+        <div style={{ textAlign: "right" }}>
           <Link to="/streams/new" className="ui button primary">
             Create Stream
           </Link>
@@ -57,15 +61,12 @@ class StreamList extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     streams: Object.values(state.streams),
     currentUserId: state.auth.userId,
-    isSignedIn: state.auth.isSignedIn
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchStreams }
-)(StreamList);
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
